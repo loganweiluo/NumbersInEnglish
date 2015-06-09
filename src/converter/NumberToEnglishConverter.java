eadb0951b8e39900834286ccbef9d8a6b3533707
Wei.Luo <loganweiluo@gmail.com>
2015-06-09 15:22:08 +0100

    added the first test

--- src/converter/NumberToEnglishConverter.java
+++ src/converter/NumberToEnglishConverter.java
@@ -4,4 +4,10 @@ package converter;
  * Created by weiluo on 09/06/15.
  */
 public class NumberToEnglishConverter {
+    public String convert(int i) {
+        if(i==1){
+            return "one";
+        }
+        return null;
+    }
 }
--- src/converter/NumberToEnglishConverterTest.java
+++ src/converter/NumberToEnglishConverterTest.java
@@ -1,7 +1,18 @@
 package converter;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by weiluo on 09/06/15.
  */
 public class NumberToEnglishConverterTest {
+    NumberToEnglishConverter converter = new NumberToEnglishConverter();
+
+    @Test
+    public void shouldConvertOne() {
+        assertEquals("one", converter.convert(1));
+    }
 }
